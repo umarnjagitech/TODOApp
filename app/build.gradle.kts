d1b5416b468e19fd14ca02170f9ff3664d235e7c
Nj <92800887+omarndungo@users.noreply.github.com>
2024-05-16 13:37:36 +0300

    Fine fine project config changes, don't worry about them.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     id("androidx.room")
     id("kotlin-kapt")
+    id("org.jetbrains.kotlin.kapt")
     //id("com.google.devtools.ksp")
 }
 
@@ -77,6 +78,7 @@ dependencies {
     val room_version = "2.6.1"
 
     implementation ("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:2.6.1")
     kapt ("androidx.room:room-compiler:$room_version")
     //ksp ("androidx.room:room-compiler:$room_version")
 }
