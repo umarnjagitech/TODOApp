3dd19f3a408c005e83edd97d7448a4aa427b783c
Nj <92800887+omarndungo@users.noreply.github.com>
2024-05-18 11:38:58 +0300

    Putting together the UI using mutliple composables

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -3,48 +3,54 @@ package com.example.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.todoapp.ui.theme.TODOAppTheme
+import YOUR_PACKAGE_NAME.composables.TodoInputBar
+import YOUR_PACKAGE_NAME.composables.TodoItemsContainer
+import YOUR_PACKAGE_NAME.data.TodoItem
+import YOUR_PACKAGE_NAME.ui.constants.OverlappingHeight
+import kotlinx.coroutines.flow.flowOf
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TODOAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                TodoItemsContainer(
+                    // 1. Mock Data for Todo Items
+                    todoItemsFlow = flowOf(
+                        listOf(
+                            TodoItem(title = "Todo Item 1"),
+                            TodoItem(title = "Todo Item 2", isDone = true),
+                            TodoItem(title = "Todo Item 3"),
+                            TodoItem(title = "Todo Item 4", isDone = true),
+                            TodoItem(title = "Todo Item 5"),
+                            TodoItem(title = "Todo Item 6"),
+                            TodoItem(title = "Todo Item 7"),
+                            TodoItem(title = "Todo Item 8"),
+                            TodoItem(title = "Todo Item 9"),
+                            TodoItem(title = "Todo Item 10"),
+                            TodoItem(title = "Todo Item 11", isDone = true),
+                            TodoItem(title = "Todo Item 12"),
+                            TodoItem(title = "Todo Item 13"),
+                            TodoItem(title = "Todo Item 14"),
+                            TodoItem(title = "Todo Item 15")
+                        )
+                    ),
+                    onItemClick = {},
+                    onItemDelete = {},
+                    // 2. Space Adjustment for Overlapping UI Elements
+                    overlappingElementsHeight = OverlappingHeight
+                )
+                TodoInputBar(
+                    modifier = Modifier.align(Alignment.BottomStart),
+                    onAddButtonClick = {}
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(
-    showBackground = true,
-    showSystemUi = true
-)
-@Composable
-fun GreetingPreview() {
-    TODOAppTheme {
-        Greeting("Android")
     }
 }
